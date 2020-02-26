a28c19baf94ac26176183f42309a92cbe3002743
HouserChiu <18391726137@163.com>
2020-02-26 22:17:05 +0800

    page

--- ChromeHeadless.py
+++ ChromeHeadless.py
@@ -10,17 +10,17 @@ from pyquery import PyQuery as pq
 from config import *
 import pymongo
 
-client=pymongo.MongoClient(MONGO_URL)
-db=client[MONGO_DB]
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
 
-
-chrome_options=Options()
+chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
-browser=webdriver.Chrome(options=chrome_options)
-wait=WebDriverWait(browser, 10)
+browser = webdriver.Chrome(options=chrome_options)
+wait = WebDriverWait(browser, 10)
 
-browser.set_window_size(1400,900)
+browser.set_window_size(1400, 900)
+
 
 def search():
     print('正在搜索')
@@ -29,45 +29,45 @@ def search():
         input = wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '#q'))
         )
-        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_TSearchForm > div.search-button > button')))
+        submit = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button')))
         input.send_keys('美食')
         submit.click()
-        button = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="J_Quick2Static"]')))
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="J_Quick2Static"]')))
         button.click()
-        wb = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_OtherLogin > a.weibo-login')))
+        wb = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_OtherLogin > a.weibo-login')))
         wb.click()
         username = wait.until(EC.presence_of_all_elements_located(
             (By.CSS_SELECTOR, '#pl_login_logged > div > div:nth-child(2) > div > input')))
         password = wait.until(EC.presence_of_all_elements_located(
             (By.CSS_SELECTOR, '#pl_login_logged > div > div:nth-child(3) > div > input')))
-        submit = wait.until(EC.element_to_be_clickable(
+        login = wait.until(EC.element_to_be_clickable(
             (By.CSS_SELECTOR, '#pl_login_logged > div > div:nth-child(7) > div:nth-child(1) > a > span')))
         username[0].send_keys('your username')
         password[0].send_keys('your password')
-        submit.click()
+        login.click()
         time.sleep(5)
-        submit = wait.until(EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, '#pl_login_logged > div > div:nth-child(7) > div:nth-child(1) > a > span')))
-        submit.click()
+
         get_products()
     except ElementNotInteractableException:
         return search()
     except TimeoutException:
         return search()
 
+
 def get_products():
-    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'#mainsrp-itemlist .items .item')))
-    html=browser.page_source
-    doc=pq(html)
-    items=doc('#mainsrp-itemlist .items .item').items()
+    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '#mainsrp-itemlist .items .item')))
+    html = browser.page_source
+    doc = pq(html)
+    items = doc('#mainsrp-itemlist .items .item').items()
     for item in items:
-        product={
-            'image':item.find('.pic .img').attr('src'),
-            'price':item.find('.price').text(),
-            'deal':item.find('.deal-cnt').text()[:-3],
-            'title':item.find('.title').text(),
-            'shop':item.find('.shop').text(),
-            'location':item.find('.location').text()
+        product = {
+            'image': item.find('.pic .img').attr('src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('.deal-cnt').text()[:-3],
+            'title': item.find('.title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
         }
         print(product)
         save_to_mongo(product)
@@ -76,16 +76,16 @@ def get_products():
 def save_to_mongo(result):
     try:
         if db[MONGO_TABLE].insert(result):
-            print('存储到MONGODB成功',result)
+            print('存储到MONGODB成功', result)
     except Exception:
         print('存储到MONGODB失败', result)
 
 
 def main():
-    total=search()
+    total = search()
     print(total)
     browser.close()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- chrome.py
+++ chrome.py
@@ -1,3 +1,4 @@
+import re
 from time import sleep
 
 from selenium import webdriver
@@ -9,11 +10,12 @@ from pyquery import PyQuery as pq
 from config import *
 import pymongo
 
-client=pymongo.MongoClient(MONGO_URL)
-db=client[MONGO_DB]
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
 
-browser=webdriver.Chrome()
-wait=WebDriverWait(browser, 10)
+browser = webdriver.Chrome()
+wait = WebDriverWait(browser, 10)
+
 
 def search():
     print('正在搜索')
@@ -22,45 +24,65 @@ def search():
         input = wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '#q'))
         )
-        submit=wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_TSearchForm > div.search-button > button')))
+        submit = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button')))
         input.send_keys('美食')
         submit.click()
-        button = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="J_Quick2Static"]')))
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="J_Quick2Static"]')))
         button.click()
-        wb = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_OtherLogin > a.weibo-login')))
+        wb = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_OtherLogin > a.weibo-login')))
         wb.click()
         username = wait.until(EC.presence_of_all_elements_located(
             (By.CSS_SELECTOR, '#pl_login_logged > div > div:nth-child(2) > div > input')))
         password = wait.until(EC.presence_of_all_elements_located(
             (By.CSS_SELECTOR, '#pl_login_logged > div > div:nth-child(3) > div > input')))
-        submit = wait.until(EC.element_to_be_clickable(
+        login = wait.until(EC.element_to_be_clickable(
             (By.CSS_SELECTOR, '#pl_login_logged > div > div:nth-child(7) > div:nth-child(1) > a > span')))
         username[0].send_keys('your username')
         password[0].send_keys('your password')
-        submit.click()
+        login.click()
         sleep(5)
-        submit = wait.until(EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, '#pl_login_logged > div > div:nth-child(7) > div:nth-child(1) > a > span')))
-        submit.click()
+        total = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.total')))
         get_products()
+        return total.text
+
     except ElementNotInteractableException:
         return search()
     except TimeoutException:
         return search()
 
+
+def next_page(page_number):
+    try:
+        input = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.form > input'))
+        )
+        submit = wait.until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.form > span.btn.J_Submit')))
+        input.clear()
+        input.send_keys(page_number)
+        submit.click()
+        wait.until(EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > ul > li.item.active > span'), str(page_number)))
+        get_products()
+    except TimeoutException:
+        next_page(page_number)
+
+
 def get_products():
-    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'#mainsrp-itemlist .items .item')))
-    html=browser.page_source
-    doc=pq(html)
-    items=doc('#mainsrp-itemlist .items .item').items()
+    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '#mainsrp-itemlist .items .item')))
+    html = browser.page_source
+    doc = pq(html)
+    items = doc('#mainsrp-itemlist .items .item').items()
     for item in items:
-        product={
-            'image':item.find('.pic .img').attr('src'),
-            'price':item.find('.price').text(),
-            'deal':item.find('.deal-cnt').text()[:-3],
-            'title':item.find('.title').text(),
-            'shop':item.find('.shop').text(),
-            'location':item.find('.location').text()
+        product = {
+            'image': item.find('.pic .img').attr('src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('.deal-cnt').text()[:-3],
+            'title': item.find('.title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
         }
         print(product)
         save_to_mongo(product)
@@ -68,17 +90,19 @@ def get_products():
 
 def save_to_mongo(result):
     try:
-        if db[MONGO_TABLE].insert(result):
-            print('存储到MONGODB成功',result)
+        if db[MONGO_TABLE].insert_many(result):
+            print('存储到MONGODB成功', result)
     except Exception:
         print('存储到MONGODB失败', result)
 
 
 def main():
-    total=search()
-    print(total)
+    total = search()
+    total = int(re.compile('(\d+)').search(total).group(1))
+    for i in range(2, total + 1):
+        next_page(i)
     browser.close()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
